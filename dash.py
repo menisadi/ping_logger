f6735f3bb47a52233050c0140c7bae047ed560af
menisadi <menisadi@gmail.com>
2023-07-23 09:29:37 +0300

    linting

--- dash.py
+++ dash.py
@@ -1,16 +1,17 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
 
 df = pd.read_csv("pings.csv")
-df['5'] = 5 # for the time-out pings to be visible on the plot 
+df['5'] = 5  # for the time-out pings to be visible on the plot
 fig = go.Figure()
 
-trace_good = px.scatter(df[df['delay'] != np.inf], x='time', y='delay', log_y=True)
-trace_bad = px.scatter(df[df['delay'] == np.inf], x='time', y='5', log_y=True)
+trace_good = px.scatter(df[df['delay'] != np.inf],
+                        x='time', y='delay', log_y=True)
+trace_bad = px.scatter(df[df['delay'] == np.inf],
+                       x='time', y='5', log_y=True)
 trace_bad.update_traces(marker=dict(color='red', symbol='x'))
 
 fig.add_trace(trace_good.data[0])
