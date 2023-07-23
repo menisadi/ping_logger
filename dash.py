10bc0d51de95debfe813e74f370cae28cdec2b2a
menisadi <menisadi@gmail.com>
2023-07-23 20:19:13 +0300

    html for online use - smaller

--- dash.py
+++ dash.py
@@ -5,25 +5,24 @@ import plotly.graph_objects as go
 
 
 df = pd.read_csv("pings.csv")
-# convert 'time' to datetime
-df['time'] = pd.to_datetime(df['time'])
+
+# convert the time column to datetime
+df["time"] = pd.to_datetime(df["time"])
 # keep only the last 3 days
 # first create a datetime object for 3 days ago
-three_days_ago = pd.to_datetime('now') - pd.Timedelta(days=3)
-df = df[df['time'] > three_days_ago]
+three_days_ago = pd.to_datetime("now") - pd.Timedelta(days=1)
+df = df[df["time"] > three_days_ago]
 
-df['5'] = 5  # for the time-out pings to be visible on the plot
+df["5"] = 5  # for the time-out pings to be visible on the plot
 fig = go.Figure()
 
-trace_good = px.scatter(df[df['delay'] != np.inf],
-                        x='time', y='delay', log_y=True)
-trace_bad = px.scatter(df[df['delay'] == np.inf],
-                       x='time', y='5', log_y=True)
-trace_bad.update_traces(marker=dict(color='red', symbol='x'))
+trace_good = px.scatter(df[df["delay"] != np.inf], x="time", y="delay", log_y=True)
+trace_bad = px.scatter(df[df["delay"] == np.inf], x="time", y="5", log_y=True)
+trace_bad.update_traces(marker=dict(color="red", symbol="x"))
 
 fig.add_trace(trace_good.data[0])
 fig.add_trace(trace_bad.data[0])
 fig.update_layout(yaxis_type="log")
 
 # fig.show()
-fig.write_html("pings.html")
+fig.write_html("pings.html", include_plotlyjs=False)
