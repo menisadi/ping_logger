126cf5a3aca4665e6b28bbfa25f6c1069eef3890
menisadi <menisadi@gmail.com>
2023-07-24 19:31:19 +0300

    formatting code

--- my_dash.py
+++ my_dash.py
@@ -4,17 +4,19 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-
 df = pd.read_csv("pings.csv")
 
 # Convert the time column to datetime
 df["time"] = pd.to_datetime(df["time"])
 
-# Add a column with constant value 5 for the time-out pings to be visible on the plot
+# Add a column with constant value 5
+# for the time-out pings to be visible on the plot
 df["5"] = 5
 
 # Create scatter plot for good pings (delays that are not infinity)
-trace_good = px.scatter(df[df["delay"] != np.inf], x="time", y="delay", log_y=True)
+trace_good = px.scatter(
+    df[df["delay"] != np.inf], x="time", y="delay", log_y=True
+)
 
 # Create scatter plot for bad pings (delays that are infinity)
 trace_bad = px.scatter(df[df["delay"] == np.inf], x="time", y="5", log_y=True)
@@ -22,9 +24,11 @@ trace_bad.update_traces(marker=dict(color="red", symbol="x"))
 
 # Calculate median delay for each hour of the day
 df["hour"] = df["time"].dt.hour
-median_delay_per_hour = df[df["delay"] != np.inf].groupby("hour")["delay"].median()
+groupby_hour = df[df["delay"] != np.inf].groupby("hour")
+median_delay_per_hour = groupby_hour["delay"].median()
 
-# Calculate median delay for each combination of day of the week and hour of the day
+# Calculate median delay for each combination of day of the week
+# and hour of the day
 df["day_of_week"] = df["time"].dt.dayofweek
 df_filtered = df[(df["delay"] != np.inf) & (~df["day_of_week"].isin([4, 5]))]
 median_delay_per_day_hour = (
@@ -42,7 +46,9 @@ remapping_days = {
 }
 median_delay_per_day_hour = median_delay_per_day_hour.rename(remapping_days)
 # Create bar plot for median delay per hour
-bar_trace = go.Bar(x=median_delay_per_hour.index, y=median_delay_per_hour.values)
+bar_trace = go.Bar(
+    x=median_delay_per_hour.index, y=median_delay_per_hour.values
+)
 
 # Create heatmap plot for median delay per day of the week and hour of the day
 heatmap_trace = go.Heatmap(
@@ -68,9 +74,13 @@ fig.update_layout(yaxis_type="log", yaxis2_type="log")
 fig.update_yaxes(title_text="Ping Delay (log scale)", row=1, col=1)
 fig.update_yaxes(title_text="Median Delay (log scale)", row=2, col=1)
 fig.update_xaxes(title_text="Time", row=2, col=1)
-fig.update_xaxes(title_text="Hour of the Day", row=2, col=1, dtick=1, tickformat="%H")
+fig.update_xaxes(
+    title_text="Hour of the Day", row=2, col=1, dtick=1, tickformat="%H"
+)
 fig.update_yaxes(title_text="Day of Week", row=3, col=1, dtick=1, tick0=1)
-fig.update_xaxes(title_text="Hour of the Day", row=3, col=1, dtick=1, tickformat="%H")
+fig.update_xaxes(
+    title_text="Hour of the Day", row=3, col=1, dtick=1, tickformat="%H"
+)
 fig.update_traces(showlegend=False)
 fig.update_coloraxes(showscale=False)
 
